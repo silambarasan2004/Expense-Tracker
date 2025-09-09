19af2b7e8bc60bb642c0542bf83f7bf50f4bfd2c
Silambarasan E <silambarasane920@gmail.com>
2025-09-09 18:41:11 +0530

    axios instance error solved

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,5 +1,5 @@
 import { API_PATHS } from "./apiPaths";
-import axiosInstance from "./axiosInstance";
+import axiosInstance from "./axiosinstance";
 
 const uploadImage = async (imageFile ) => {
     const formData = new FormData();
